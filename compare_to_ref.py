c014437da703838e9592f9a17d1bbf1717fb6d03
Richard Neher <richard@ag-neher-xps13.(none)>
2013-08-04 18:55:59 +0200

    little script to show smoothed differences with reference

---
+++ compare_to_ref.py
@@ -0,0 +1,53 @@
+from Bio import AlignIO
+import numpy as np
+import matplotlib.pyplot as plt
+
+fname = 'alignment_all_with_NL4-3_aligned_with_BAL.fasta'
+all_refs = ['B.US.', 'B.FR.', 'gi|19']
+ref = all_refs[2]
+
+smoothing=200
+
+with open(fname, 'r') as infile:
+    aln = AlignIO.read(infile, 'fasta')
+
+seqs = np.zeros((len(aln)-len(all_refs), aln.get_alignment_length()), dtype='|S1')
+seq_ids = []
+si=0
+for seq in aln:
+    if seq.id[:len(ref)]==ref:
+        print 'ref:', seq.id
+        ref_seq = np.array(seq.seq)
+    elif seq.id[:len(ref)] not in all_refs:
+        print 'query:', seq.id
+        seqs[si,:] = np.array(seq.seq)
+        seq_ids.append(seq.id)
+        si+=1
+
+plt.figure()
+plt.title('total difference')
+for si,seq in enumerate(seqs):
+    plt.plot(np.convolve(np.ones(smoothing, float)/smoothing, seq!=ref_seq), label = seq_ids[si])
+
+plt.xlabel('alignment coordinate')
+plt.ylabel('smoothed difference')
+plt.legend(loc=2)
+
+plt.figure()
+plt.title('snp difference')
+for si,seq in enumerate(seqs):
+    plt.plot(np.convolve(np.ones(smoothing, float)/smoothing, (seq!=ref_seq)*(seq!='-')*(ref_seq!='-')), label = seq_ids[si])
+
+plt.xlabel('alignment coordinate')
+plt.ylabel('smoothed difference')
+plt.legend(loc=2)
+
+plt.figure()
+plt.title('indels difference')
+for si,seq in enumerate(seqs):
+    plt.plot(np.convolve(np.ones(smoothing, float)/smoothing, (seq!=ref_seq)*((seq=='-')+(ref_seq=='-')>0)), label = seq_ids[si])
+
+plt.xlabel('alignment coordinate')
+plt.ylabel('smoothed difference')
+plt.legend(loc=2)
+
