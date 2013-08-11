1408f77b17f3fe01b3c2903213a6320941c8c1c4
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2013-08-11 20:42:16 +0200

    Demultiplex updated and additional info modules

---
+++ datasets.py
@@ -0,0 +1,16 @@
+# vim: fdm=indent
+'''
+author:     Fabio Zanini
+date:       11/08/13
+content:    Description module for our HIV datasets.
+'''
+# Globals
+dataset_testmiseq = {'description': 'Test run for the MiSeq and the PCR',
+                     'adapters': (02, 04, 07, 16, 18, 19),
+                     'samples': ('NL4-3', 'SF162', 'F10',
+                                 'patient 37024',
+                                 'MIX1 SF162 50% + NL4-3 50%',
+                                 'MIX2 SF162 95% + NL4-3 4.5% + F10 0.5%'),
+                     'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/run28_test_samples/',
+                     'date': '2013-07-30',
+                     'comments': 'MiSeq run28'}
