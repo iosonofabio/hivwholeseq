f23de573f5b1dd8eebc979c1e1becb5ab1a8b79b
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2013-09-13 14:55:25 +0200

    Trim reads during post-mapping filtering

--- mapping/filter_mapped_reads.py
+++ mapping/filter_mapped_reads.py
@@ -10,6 +10,7 @@ import os
 import sys
 import argparse
 import cPickle as pickle
+from operator import itemgetter
 from collections import defaultdict
 from itertools import izip
 import pysam
@@ -22,13 +23,11 @@ from mapping.adapter_info import load_adapter_table, foldername_adapter
 from mapping.miseq import alpha, read_types
 from mapping.filenames import get_consensus_filename, get_mapped_filename
 from mapping.mapping_utils import get_ind_good_cigars, convert_sam_to_bam,\
-        pair_generator
+        pair_generator, get_range_good_cigars
 
 
 
 # Globals
-VERBOSE = 1
-
 # FIXME
 from mapping.datasets import dataset_testmiseq as dataset
 data_folder = dataset['folder']
@@ -58,7 +57,7 @@ def fork_self(data_folder, adaID, fragment, VERBOSE=0):
                  '-S', '/bin/bash',
                  '-o', JOBLOGOUT,
                  '-e', JOBLOGERR,
-                 '-N', 'fmr '+'{:02d}'.format(adaID),
+                 '-N', 'fmr '+'{:02d}'.format(adaID)+' '+fragment,
                  '-l', 'h_rt='+cluster_time,
                  '-l', 'h_vmem='+vmem,
                  JOBSCRIPT,
@@ -98,9 +97,11 @@ def filter_reads(data_folder, adaID, fragment, VERBOSE=0):
             n_good = 0
             n_unmapped = 0
             n_unpaired = 0
+            n_mutator = 0
             n_mismapped_edge = 0
+            n_badcigar = 0
             for i_pairs, reads in enumerate(pair_generator(bamfile)):
-            
+
                 # Limit to the first reads
                 if 2 * i_pairs >= maxreads: break
             
@@ -129,32 +130,112 @@ def filter_reads(data_folder, adaID, fragment, VERBOSE=0):
                     skip = True
 
                 else:
-            
-                    # Mismappings are often characterized by a large number of mutations,
-                    # and/or an overhang before the beginning of the fragment or beyond 
-                    # its end
-                    muts = []
+
+                    # Mismappings are often characterized by many mutations:
+                    # check the number of mismatches and skip reads with too many
+                    mm = (dict(read1.tags)['NM'], dict(read2.tags)['NM'])
+                    if (max(mm) > 50) or (sum(mm) > 50):
+                        if VERBOSE >= 2:
+                            print 'Read pair '+read.qname+': too many mismatches '+\
+                                    '('+str(mm[0])+' + '+str(mm[1])+')'
+                        n_mutator += 1
+                        skip = True
+
+                    else:
+    
+                        # Mismappings are sometimes at fragment edges:
+                        # Check for overhangs beyond the edge
+                        for read in reads:
+                            # Check overhangs
+                            read_start = read.pos
+                            read_end = read.pos + sum(x[1] for x in read.cigar if x[0] != 1)
+                            if (((read_start == 0) and (read.cigar[0][0] == 1)) or
+                                ((read_end == len(ref)) and (read.cigar[-1][0] == 1))):
+                                n_mismapped_edge += 1
+                                skip = True
+                                break
+
+                # If the read pair survived, check and trim good cigars
+                if not skip:
+                    rlens = []
+                    tampered = False
                     for read in reads:
-            
-                        # Check overhangs
-                        read_start = read.pos
-                        read_end = read.pos + sum(x[1] for x in read.cigar if x[0] != 1)
-                        if (((read_start == 0) and (read.cigar[0][0] == 1)) or
-                            ((read_end == len(ref)) and (read.cigar[-1][0] == 1))):
-                            n_mismapped_edge += 1
+                        cigar = read.cigar
+                        len_cig = len(cigar)
+                        (good_cigars, first_good_cigar, last_good_cigar) = \
+                                get_ind_good_cigars(cigar, match_len_min=match_len_min,
+                                                    full_output=True)
+
+                        # If there are no good CIGARs, skip
+                        if not good_cigars.any():
+                            n_badcigar += 1
                             skip = True
                             break
 
+                        # If all CIGARs are fine, no tampering needed
+                        elif good_cigars.all():
+                            rlens.append(read.rlen)
+                            continue
+
+                        # We tamper with the read
+                        tampered = True
+
+                        # Get the good CIGARs coordinates
+                        ((start_read, end_read), (start_ref, end_ref)) = \
+                                get_range_good_cigars(cigar, read.pos,
+                                                      match_len_min=match_len_min,
+                                                      trim_left=trim_bad_cigars,
+                                                      trim_right=trim_bad_cigars)
+
+                        # Modify the read in place
+                        # The mate stuff is done afterwards, and so is isize
+                        # Note: we must take the qual BEFORE changing the seq,
+                        # THEN change the seq, THEN change the qual... !!
+                        seq = read.seq
+                        qual = read.qual
+                        read.seq = seq[start_read: end_read]
+                        read.qual = qual[start_read: end_read]
+                        read.pos = start_ref
+                        # Give up the number of mismatches
+                        read.tags.pop(map(itemgetter(0), read.tags).index('NM'))
+                        # CIGAR
+                        cigarnew = cigar[first_good_cigar: last_good_cigar + 1]
+                        # Trim cigar block lengths
+                        if first_good_cigar != 0:
+                            cigarnew[0] = (cigarnew[0][0],
+                                           cigarnew[0][1] - trim_bad_cigars)
+                        if last_good_cigar != len_cig - 1:
+                            cigarnew[-1] = (cigarnew[-1][0],
+                                            cigarnew[-1][1] - trim_bad_cigars)
+                        read.cigar = cigarnew
+                        rlens.append(end_read - start_read)
+
+                    # Mate pair stuff if we tampered with any of the reads
+                    if (not skip) and tampered:
+                        read1.mpos = read2.pos
+                        read2.mpos = read1.pos
+
+                        # Insert size
+                        readf = reads[read1.is_reverse]
+                        readr = reads[read2.is_reverse]
+                        isize = max([read1.pos + rlens[0],
+                                     read2.pos + rlens[1]]) -\
+                                min([read1.pos, read2.pos])
+                        readf.isize = isize
+                        readr.isize = -isize
+
                 # Write the output
-                if not skip:
+                if skip:
+                    map(trashfile.write, reads)
+                else:
                     n_good += 1
                     map(outfile.write, reads)
-                else:
-                    map(trashfile.write, reads)
+
 
     if VERBOSE >= 3:
         print 'Reads: '+str(n_good)+' good, '+str(n_unmapped)+' unmapped, '+\
-                str(n_unpaired)+' unpaired, '+str(n_mismapped_edge)+' edge.'
+                str(n_unpaired)+' unpaired, '+str(n_mismapped_edge)+' edge, '+\
+                str(n_mutator)+' many-mutations, '+str(n_badcigar)+' bad CIGAR.'
 
 
 
