074f852f8ec5a8bc1e052b9ee91bb30ce01188b5
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2014-01-05 15:36:21 +0100

    Use Pandas for sample bookkeeping (still back compatible)

--- hivwholeseq/datasets.py
+++ hivwholeseq/datasets.py
@@ -4,86 +4,62 @@ author:     Fabio Zanini
 date:       11/08/13
 content:    Description module for our HIV datasets.
 '''
+# Modules
+from hivwholeseq.samples import sample_table
+
+
 # Globals
-test_tiny = {'description': 'Small subsample of run 37 for testing purposes',
-             'date': '2013-12-06',
-             'n_cycles': 500,
-             'comments': 'MiSeq run37 (tiny subsample)',
-             'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/test_tiny/',
-             'raw_data': {'read1': 'read1.fastq',
-                          'adapter': 'barcode.fastq',
-                          'read2': 'read2.fastq'},
-             'adapters': ('TS2',  'TS4',  'TS5',  'TS6', 'TS7',
-                          'TS12', 'TS13', 'TS14', 'TS15'),
-             'samples': ('VK04-3106-test',
-                         '08HR-0235-test',
-                         'VK07-4778-test',
-                         'VK03-4298-test',
-                         'VK09-7738-test',
-                         'VK08-8014-test',
-                         'VK08-6634-test',
-                         'VK07-8262-test',
-                         'VK08-2987-test',
-                        ),
-            }
+MiSeq_runs_list = [\
+ {'name': 'test_tiny',
+  'description': 'Small subsample of run 37 for testing purposes',
+  'date': '2013-12-06',
+  'n_cycles': 500,
+  'comments': 'MiSeq run37 (tiny subsample)',
+  'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/test_tiny/',
+  'raw_data': {'read1': 'read1.fastq',
+               'adapter': 'barcode.fastq',
+               'read2': 'read2.fastq'},
+ },
 
+ {'name': 'Tue28',
+  'description': 'Test run for the MiSeq and the PCR',
+  'date': '2013-07-30',
+  'n_cycles': 500,
+  'comments': 'MiSeq run28',
+  'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/run28_test_samples/',
+  'raw_data': {'read1': 'lane1_NoIndex_L001_R1_001.fastq',
+               'adapter': 'lane1_NoIndex_L001_R2_001.fastq',
+               'read2': 'lane1_NoIndex_L001_R3_001.fastq'},
+ },
 
-testmiseq = {'description': 'Test run for the MiSeq and the PCR',
-             'date': '2013-07-30',
-             'n_cycles': 500,
-             'comments': 'MiSeq run28',
-             'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/run28_test_samples/',
-             'raw_data': {'read1': 'lane1_NoIndex_L001_R1_001.fastq',
-                          'adapter': 'lane1_NoIndex_L001_R2_001.fastq',
-                          'read2': 'lane1_NoIndex_L001_R3_001.fastq'},
-             'adapters': ('TS2', 'TS4', 'TS7', 'TS16', 'TS18', 'TS19'),
-             'samples': ('NL4-3',
-                         'SF162',
-                         'F10',
-                         '37024',
-                         'MIX1',
-                         'MIX2',
-                        ),
-            }
+ {'name': 'Tue37',
+  'description': 'Second library (first patient-only)',
+  'date': '2013-09-30',
+  'n_cycles': 500,
+  'comments': 'MiSeq run37',
+  'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/run37/',
+  'raw_data': {'read1': 'read1.fastq',
+               'adapter': 'barcode.fastq',
+               'read2': 'read2.fastq'},
+ },
 
-patients1 = {'description': 'Second library (first patient-only)',
-             'date': '2013-09-30',
-             'n_cycles': 500,
-             'comments': 'MiSeq run37',
-             'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/run37/',
-             'raw_data': {'read1': 'read1.fastq',
-                          'adapter': 'barcode.fastq',
-                          'read2': 'read2.fastq'},
-             'adapters': ('TS2', 'TS4', 'TS5', 'TS6', 'TS7',
-                          'TS12', 'TS13', 'TS14', 'TS15'),
-             'samples': ('VK04-3106',
-                         '08HR-0235',
-                         'VK07-4778',
-                         'VK03-4298',
-                         'VK09-7738',
-                         'VK08-8014',
-                         'VK08-6634',
-                         'VK07-8262',
-                         'VK08-2987',
-                        ),
-            }
+ {'name': 'Lina_nextera',
+  'description': 'Test run for the Nextera library (Sweden)',
+  'date': '2013-10-20',
+  'n_cycles': 300,
+  'comments': 'Library prepared by Lina, sequencing by Pawel Zajac (illumina)',
+  'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/pawel_nextera/',
+  'raw_data': None, # ALREADY DEMULTIPLEXED!
+ },
 
+]
 
-testnextera_Lina = {'description': 'Test run for the Nextera library (Sweden)',
-                    'date': '2013-10-20',
-                    'n_cycles': 300,
-                    'comments': 'Library prepared by Lina, sequencing by Pawel Zajac (illumina)',
-                    'folder': '/ebio/ag-neher/share/data/MiSeq_HIV_Karolinska/pawel_nextera/',
-                    'raw_data': None, # ALREADY DEMULTIPLEXED!
-                    'adapters': ('01', '02'), # FAKE NUMBERS!
-                    'samples': ('Nextera_HIV-8262-1',
-                                'Nextera_HIV-8262-2'),
-                   }
+# Add samples to the runs
+for run in MiSeq_runs_list:
+    tmp = sample_table.filter_seq_run(run['name'])
+    run['adapters'] = tuple(tmp['adaID'])
+    run['samples'] = tuple(tmp['name'])
+    run['sample_table'] = tmp.copy()
 
-
-# MiSeq runs
-MiSeq_runs = {'Tue28': testmiseq,
-              'Tue37': patients1,
-              'Lina_nextera': testnextera_Lina,
-              'test_tiny': test_tiny,
-             }
+# MiSeq runs dictionary
+MiSeq_runs = {run['name']: run for run in MiSeq_runs_list}
--- hivwholeseq/samples.py
+++ hivwholeseq/samples.py
@@ -9,69 +9,94 @@ content:    Description module for HIV samples from patients.
             table is up-to-date. Better write down the machine-relevant info
             manually down here (until otherwise specified).
 '''
+# Modules
+import pandas as pd
+
+
+
+# Classes
+class SampleTable(pd.DataFrame):
+    def filter_seq_run(self, run):
+        return self[self.run == run]
+
+
+
 # Globals
 # Date is YYYY-M-D
-samples = {'VK04-3106': {'run': 'Tue37', 'adaID': 'TS2', 'date': '2004-7-9',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           '08HR-0235': {'run': 'Tue37', 'adaID': 'TS4', 'date': '2008-2-7',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'VK07-4778': {'run': 'Tue37', 'adaID': 'TS5', 'date': '2007-7-12',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'VK03-4298': {'run': 'Tue37', 'adaID': 'TS6', 'date': '2003-9-30',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'VK09-7738': {'run': 'Tue37', 'adaID': 'TS7', 'date': '2009-9-24',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK08-8014': {'run': 'Tue37', 'adaID': 'TS12', 'date': '2008-10-28',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK08-6634': {'run': 'Tue37', 'adaID': 'TS13', 'date': '2008-9-11',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK07-8262': {'run': 'Tue37', 'adaID': 'TS14', 'date': '2007-11-27',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK08-2987': {'run': 'Tue37', 'adaID': 'TS15', 'date': '2008-4-21',
-                         'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+sample_list = [\
+  {'name': 'VK04-3106', 'run': 'Tue37', 'adaID': 'TS2', 'date': '2004-7-9',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': '08HR-0235', 'run': 'Tue37', 'adaID': 'TS4', 'date': '2008-2-7',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'VK07-4778', 'run': 'Tue37', 'adaID': 'TS5', 'date': '2007-7-12',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'VK03-4298', 'run': 'Tue37', 'adaID': 'TS6', 'date': '2003-9-30',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'VK09-7738', 'run': 'Tue37', 'adaID': 'TS7', 'date': '2009-9-24',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK08-8014', 'run': 'Tue37', 'adaID': 'TS12', 'date': '2008-10-28',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK08-6634', 'run': 'Tue37', 'adaID': 'TS13', 'date': '2008-9-11',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK07-8262', 'run': 'Tue37', 'adaID': 'TS14', 'date': '2007-11-27',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK08-2987', 'run': 'Tue37', 'adaID': 'TS15', 'date': '2008-4-21',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
 
-           'VK04-3106-test': {'run': 'test_tiny', 'adaID': 'TS2',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           '08HR-0235-test': {'run': 'test_tiny', 'adaID': 'TS4',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'VK07-4778-test': {'run': 'test_tiny', 'adaID': 'TS5',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'VK03-4298-test': {'run': 'test_tiny', 'adaID': 'TS6',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'VK09-7738-test': {'run': 'test_tiny', 'adaID': 'TS7',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK08-8014-test': {'run': 'test_tiny', 'adaID': 'TS12',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK08-6634-test': {'run': 'test_tiny', 'adaID': 'TS13',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK07-8262-test': {'run': 'test_tiny', 'adaID': 'TS14',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
-           'VK08-2987-test': {'run': 'test_tiny', 'adaID': 'TS15',
-                              'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK04-3106-test', 'run': 'test_tiny', 'adaID': 'TS2',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': '08HR-0235-test', 'run': 'test_tiny', 'adaID': 'TS4',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'VK07-4778-test', 'run': 'test_tiny', 'adaID': 'TS5',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'VK03-4298-test', 'run': 'test_tiny', 'adaID': 'TS6',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'VK09-7738-test', 'run': 'test_tiny', 'adaID': 'TS7',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK08-8014-test', 'run': 'test_tiny', 'adaID': 'TS12',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK08-6634-test', 'run': 'test_tiny', 'adaID': 'TS13',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK07-8262-test', 'run': 'test_tiny', 'adaID': 'TS14',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
+  {'name': 'VK08-2987-test', 'run': 'test_tiny', 'adaID': 'TS15',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i')},
 
-           'NL4-3': {'run': 'Tue28', 'adaID': 'TS2',
-                     'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'SF162': {'run': 'Tue28', 'adaID': 'TS4',
-                     'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           'F10':   {'run': 'Tue28', 'adaID': 'TS7',
-                     'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
-           '37024': {'run': 'Tue28', 'adaID': 'TS16',
-                     'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i'),
-                     'description': 'patient 37024'},
-           'MIX1':  {'run': 'Tue28', 'adaID': 'TS18',
-                     'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i'),
-                     'description': 'MIX1 SF162 50% + NL4-3 50%'},
-           'MIX2':  {'run': 'Tue28', 'adaID': 'TS19',
-                     'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i'),
-                     'description': 'MIX2 SF162 95% + NL4-3 4.5% + F10 0.5%'},
+  {'name': 'NL4-3', 'run': 'Tue28', 'adaID': 'TS2',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'SF162', 'run': 'Tue28', 'adaID': 'TS4',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': 'F10', 'run': 'Tue28', 'adaID': 'TS7',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i')},
+  {'name': '37024', 'run': 'Tue28', 'adaID': 'TS16',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i'),
+   'description': 'patient 37024'},
+  {'name': 'MIX1', 'run': 'Tue28', 'adaID': 'TS18',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i'),
+   'description': 'MIX1 SF162 50% + NL4-3 50%'},
+  {'name': 'MIX2', 'run': 'Tue28', 'adaID': 'TS19',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5bi', 'F6i'),
+   'description': 'MIX2 SF162 95% + NL4-3 4.5% + F10 0.5%'},
 
-           'Nextera_HIV-8262-1': {'run': 'testnextera_Lina', 'adaID': 'N1', # WE DO NOT KNOW THE ADAID
-                                  'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i'),
-                                  'description': 'HIV-8262-1: 0.2 ng/ul'},
-           'Nextera_HIV-8262-2': {'run': 'testnextera_Lina', 'adaID': 'N2', # WE DO NOT KNOW THE ADAID
-                                  'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i'),
-                                  'description': 'HIV-8262-2: 0.033 ng/ul'},
-}
+  # WE DO NOT KNOW THE ADAID OF THESE TWO
+  {'name': 'Nextera_HIV-8262-1', 'run': 'testnextera_Lina', 'adaID': '01',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i'),
+   'description': 'HIV-8262-1: 0.2 ng/ul'},
+  {'name': 'Nextera_HIV-8262-2', 'run': 'testnextera_Lina', 'adaID': '02',
+   'fragments': ('F1i', 'F2i', 'F3i', 'F4i', 'F5ai', 'F6i'),
+   'description': 'HIV-8262-2: 0.033 ng/ul'},
+  ]
+
+# Make a dictionary (deprecated)
+samples = {}
+for line in sample_list:
+    dic = dict(line)
+    name = dic.pop('name')
+    samples[name] = dic
+
+
+# Sample table using Pandas
+sample_table = SampleTable(sample_list)
 
 
 
