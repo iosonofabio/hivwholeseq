af7cdbdd279bd5ce4b85fb9335e7def1ff619034
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2014-10-31 19:05:38 +0100

    More streamlined logit class

--- hivwholeseq/plot_utils.py
+++ hivwholeseq/plot_utils.py
@@ -8,7 +8,7 @@ content:    Support module to add useful plot functions, e.g. logit scales.
 import numpy as np
 from matplotlib import transforms as mtransforms
 from matplotlib.ticker import Formatter, FixedLocator
-from matplotlib.ticker import NullFormatter, ScalarFormatter
+from matplotlib.ticker import NullFormatter, ScalarFormatter, FuncFormatter
 from matplotlib import scale as mscale
 
 
@@ -78,6 +78,7 @@ class LogitScale(mscale.ScaleBase):
     Only base 10 ticks are implemented (e.g. [0.01, 0.1, 0.5, 0.9, 0.99]).
     """
     name = 'logit'
+    thresholds = [-10, 10]
 
 
     def __init__(self, axis, **kwargs):
@@ -95,17 +96,23 @@ class LogitScale(mscale.ScaleBase):
         """Set default ticks and labels"""
 
         # FIXME: pretty rough, ouch!
-        tickloc = np.array([0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999])
-        ticklocminor = np.concatenate([[10**po * x for x in xrange(2 , 10)]
-                                       for po in xrange(-4, -1)] + \
-                                      [[0.1 * x for x in xrange(2 , 9)]] + \
-                                      [[1 - 10**po * (10 - x) for x in xrange(2, 10)]
-                                       for po in xrange(-2, -5, -1)])
+        logrange = self.thresholds
+        tickleft = np.logspace(logrange[0], -1, -logrange[0])
+        tickcenter = [0.5]
+        tickright = 1 - np.logspace(-logrange[1], -1, logrange[1])[::-1]
+        tickloc = np.concatenate([tickleft, tickcenter, tickright])
+
+        tickleft = [[10**po * x for x in xrange(2 , 10)]
+                    for po in xrange(logrange[0] - 1, -1)]
+        tickcenter = [[0.1 * x for x in xrange(2 , 9)]]
+        tickright = [[1 - 10**po * (10 - x) for x in xrange(2, 10)]
+                     for po in xrange(-2, logrange[1] - 2, -1)]
+        ticklocminor = np.concatenate(tickleft + tickcenter + tickright)
 
         axis.set_major_locator(FixedLocator(tickloc))
         axis.set_minor_locator(FixedLocator(ticklocminor))
 
-        axis.set_major_formatter(ScalarFormatter())
+        axis.set_major_formatter(FuncFormatter(logit_formatter))
         axis.set_minor_formatter(NullFormatter())
 
     
@@ -114,7 +121,23 @@ class LogitScale(mscale.ScaleBase):
         Limit the domain to values in ]0, 1[.
         """
         #TODO: this is not very easy with the stupid minpos argument only
-        return (max(vmin, 1e-10), min(vmax, 1 - 1e-10))
+        return (max(vmin, 10**(self.thresholds[0])),
+                min(vmax, 1 - 10**(-self.thresholds[1])))
+
+
+# Functions
+def logit_formatter(x, pos):
+    '''Format as 10^x for < 0.5, 1 - 10^x for > 0.5'''
+    if x < 0.4:
+        s = r'10^{'+r'{:1.0f}'.format(np.round(np.log10(x), 0))+r'}'
+
+    elif x > 1.0 - 0.4:
+        s = r'1 - 10^{'+r'{:1.0f}'.format(np.round(np.log10(1 - x), 0))+r'}'
+
+    else:
+        s = str(np.round(x, 1))
+
+    return r'$\mathdefault{'+s+r'}$'
 
 
 def finalize():
@@ -132,13 +155,15 @@ finalize()
 if __name__ == '__main__':
 
 
-    x = [0.01, 0.02, 0.1, 0.2, 0.5, 0.9, 0.98, 0.99]
+    x = [0.00001, 0.01, 0.02, 0.1, 0.2, 0.5, 0.9, 0.98, 0.99]
     y = np.arange(len(x))
 
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots()
     ax.plot(x, y)
     ax.set_xscale('logit')
+
+    ax.grid(True)
 
     plt.ion()
     plt.show()
