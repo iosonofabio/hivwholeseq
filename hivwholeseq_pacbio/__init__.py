ad1acc93aa9e5499ecd1ba189999a51088cb99ba
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2015-01-25 14:34:57 +0100

    PacBio part detached

--- hivwholeseq_pacbio/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['seqan_module',
-           'samples',
-           'datasets',
-           'get_coverage',
-           'filenames',
-           'mapping_utils',
-           'pacbio_rs_II',
-           'premap_to_reference']
