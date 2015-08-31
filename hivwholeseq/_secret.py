5dafbe44a34d8eb36dcebd35d2008cebfd08e162
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2015-08-31 17:49:38 +0200

    secret is back on this branch

---
+++ hivwholeseq/_secret.py
@@ -0,0 +1,29 @@
+# vim: fdm=indent
+'''
+author:     Fabio Zanini
+date:       31/08/15
+content:    Personal data on patients and samples that should stay secret.
+'''
+pdict = {'p1': '20097',
+         'p2': '15363',
+         'p3': '15823',
+         'p4': '15313',
+         'p5': '15376',
+         'p6': '20529',
+         'p7': '15107',
+         'p8': '9669',
+         'p9': '15241',
+         'p10': '15304',
+         'p11': '15319',
+        }
+
+samplenames_suspected_contamination = ['VK04-4187',
+                                       '14908',
+                                       '18113',
+                                       '12879',
+                                       '6154',
+                                       '18798',
+                                      ]
+
+
+samplenames_suspected_contamination_consensus = ['12879', '6154', '18798']
